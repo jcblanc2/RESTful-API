7bb132caa3f37cf02b0201dc8ce2231bd9826a4e
jcblanc2 <jclaytonblanc@gmail.com>
2023-09-27 13:35:35 -0400

    This commit adds CORS

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/post');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        
+        const allPosts = await Post.find();
+        res.send(allPosts);
     } catch (error) {
-        
+        res.send({ message: "Error!" });
+    }
+});
+
+
+router.get('/:postId', async (req, res) => {
+    try {
+        const allPosts = await Post.findById(req.params.postId);
+        res.send(allPosts);
+    } catch (error) {
+        res.send({ message: "Error!" });
     }
 });
 
@@ -20,6 +31,27 @@ router.post('/', async (req, res) => {
     try {
         const postSaved = await post.save();
         res.status(202).json(postSaved);
+    } catch (error) {
+        res.send({ message: "Error!" });
+    }
+});
+
+
+router.delete('/:postId', async (req, res) => {
+    try {
+        const postRemoved = await Post.findByIdAndDelete(req.params.postId);
+        res.json(postRemoved);
+    } catch (error) {
+        res.send({ message: "Error!" });
+    }
+});
+
+
+
+router.patch('/:postId', async (req, res) => {
+    try {
+        const postRemoved = await Post.updateOne({ _id: req.params.postId }, { $set: { title: req.body.title } });
+        res.json(postRemoved);
     } catch (error) {
         res.send({ message: "Error!" });
     }
