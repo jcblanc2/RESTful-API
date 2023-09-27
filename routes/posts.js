5484bb164ddfe41d38e0aa74f48064eb99e8903e
jcblanc2 <jclaytonblanc@gmail.com>
2023-09-27 11:44:37 -0400

    Refactor the post route to handle errors.

--- routes/posts.js
+++ routes/posts.js
@@ -3,15 +3,26 @@ const router = express.Router();
 const Post = require('../models/post');
 
 router.get('/', (req, res) => {
-    res.send("We are on post!");
+    try {
+        
+    } catch (error) {
+        
+    }
 });
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
+
+    try {
+        const postSaved = await post.save();
+        res.status(202).json(postSaved);
+    } catch (error) {
+        res.send({ message: "Error!" });
+    }
 });
 
 
